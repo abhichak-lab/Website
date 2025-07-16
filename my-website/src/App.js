5c2063269c915c17ca0fac5d36e28b36ded6cb76
abhichak-lab <abhirukthchakr@gmail.com>
2025-07-16 18:01:20 +0530

    dev init

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Abhirukth's Website
         </a>
       </header>
     </div>
