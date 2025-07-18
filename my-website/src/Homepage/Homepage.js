0a54c403ce1c6339e844d7eebfb3a89d27ef86b1
abhichak-lab <abhirukthchakr@gmail.com>
2025-07-18 19:56:18 +0530

    fonts adjusted + welcome button (half dn)

--- my-website/src/Homepage/Homepage.js
+++ my-website/src/Homepage/Homepage.js
@@ -1,6 +1,55 @@
+import * as React from "react";
 import "./Homepage.css";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 function Homepage() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["About", "Portfolio", "Prints", "Shop", "Contact"].map(
+          (text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
+      </List>
+    </Box>
+  );
+
   return (
     <div
       style={{
@@ -9,10 +58,25 @@ function Homepage() {
         height: "100vh",
       }}
     >
-      <span className="title">
-        <span className="title-name">Into the wild</span>
-        <span className="title-person-name">Abhirukth Chakravarthy</span>
-      </span>
+      <Box className="title">
+        <Box className="title-name">Into the wild</Box>
+        <Box className="title-person-name">ABHIRUKTH CHAKRAVARTHY</Box>
+      </Box>
+
+      <div>
+        {["right"].map((anchor) => (
+          <React.Fragment key={anchor}>
+            <Button onClick={toggleDrawer(anchor, true)}>Welcome</Button>
+            <Drawer
+              anchor={anchor}
+              open={state[anchor]}
+              onClose={toggleDrawer(anchor, false)}
+            >
+              {list(anchor)}
+            </Drawer>
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
